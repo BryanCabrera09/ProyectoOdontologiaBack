9ca56d1c5aabd5189b7412aac0b16ef3377cc610
Alexis <mateosiavig@gmail.com>
2023-02-01 20:41:59 -0500

    relacionPaciente

--- src/main/java/com/sm/odontologia/app/entity/Persona.java
+++ src/main/java/com/sm/odontologia/app/entity/Persona.java
@@ -2,16 +2,13 @@ package com.sm.odontologia.app.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -73,5 +70,11 @@ public class Persona implements Serializable {
 	@NotNull(message = "Telefono no puede ser nulo")
 	@Column(name = "telefono")
 	private String telefono;
+
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "persona")
+	private List<Paciente> paciente;
+
 
 }
