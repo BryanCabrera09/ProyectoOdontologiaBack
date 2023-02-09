02026046dcbc85b75707d39a45c05a6f3f85b1d1
Alexis <mateosiavig@gmail.com>
2023-02-09 16:52:42 -0500

    Cambios

--- src/main/java/com/sm/odontologia/app/service/Historial_PiezaServiceImpl.java
+++ src/main/java/com/sm/odontologia/app/service/Historial_PiezaServiceImpl.java
@@ -4,9 +4,10 @@ import com.sm.odontologia.app.entity.Historial_Pieza;
 import com.sm.odontologia.app.repository.IHistorial_PiezaDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class Historial_PiezaServiceImpl extends GenericServiceImpl<Historial_Pieza, Long> implements IHistorial_PiezaService {
 
     @Autowired
