6528e8ccb4283abe5c3461e6a5a3fe1dba015f41
Alexis <mateosiavig@gmail.com>
2023-03-03 00:05:22 -0500

    cambios

--- src/main/java/com/sm/odontologia/app/repository/IPiezaDao.java
+++ src/main/java/com/sm/odontologia/app/repository/IPiezaDao.java
@@ -13,7 +13,7 @@ public interface IPiezaDao extends JpaRepository<Pieza, Long> {
 	@Query(value = "SELECT * FROM pieza WHERE id_pieza = ?", nativeQuery = true)
 	List<Pieza> buscarPieza(Long id_pieza);
 
-	@Query(value = "Select p.id_pieza,p.fecha_consulta,p.cara_pieza,p.nombre_tratamiento, CONCAT(per.nombre,' ', per.apellido) as Nombre, o.id_odontograma,p.numero_pieza from pieza p join odontograma o on p.id_odontograma=o.id_odontograma join odontoficha f on f.id_ficha=o.id_ficha join persona per on per.id_persona=f.id_persona", nativeQuery = true)
+	@Query(value = "Select p.id_pieza,p.fecha_consulta,p.cara_pieza,p.nombre_tratamiento, per.nombre,per.apellido as Nombre, o.id_odontograma,p.numero_pieza from pieza p join odontograma o on p.id_odontograma=o.id_odontograma join odontoficha f on f.id_ficha=o.id_ficha join persona per on per.id_persona=f.id_persona", nativeQuery = true)
 	List<Pieza> buscarPiezas();
 
 }
