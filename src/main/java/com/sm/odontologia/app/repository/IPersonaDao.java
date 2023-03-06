1150132a615305f271ebef680fb6f1213d968cfc
Alexis <mateosiavig@gmail.com>
2023-03-06 10:07:33 -0500

    serviciosPacientesFicha

--- src/main/java/com/sm/odontologia/app/repository/IPersonaDao.java
+++ src/main/java/com/sm/odontologia/app/repository/IPersonaDao.java
@@ -19,4 +19,7 @@ public interface IPersonaDao extends JpaRepository<Persona, Long> {
 
 	@Query(value = "Select * from persona p where p.cedula = ?", nativeQuery = true)
 	List<Persona> buscarPersona(String cedula);
+
+	@Query(value = "SELECT * from persona p join usuario u on u.id_persona = p.id_persona join roles r on u.rol_id = r.rol_id where u.rol_id=1 and u.enabled =1",nativeQuery = true)
+	List<Persona>listarPacientes();
 }
--- src/main/java/com/sm/odontologia/app/service/IPersonaService.java
+++ src/main/java/com/sm/odontologia/app/service/IPersonaService.java
@@ -2,7 +2,11 @@ package com.sm.odontologia.app.service;
 
 import com.sm.odontologia.app.entity.Persona;
 
+import java.util.List;
+
 public interface IPersonaService extends IGenericService<Persona, Long> {
 
 	public Persona search(String cedula);
+
+	public List<Persona> listar();
 }
